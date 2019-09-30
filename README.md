3c9f065b8d8bd951cff5c2cc1f1796d7f71e47b5
Abstrct <abstract@Litebook.local>
2019-09-30 16:41:06 -0600

    First Commit of Tokens.FYI

---
+++ README.md
@@ -0,0 +1,16 @@
+# Tokens.FYI
+A tool for generating keyword lists relating to blockchain networks, exchnages, cryptocurrencies, utility coins, and other digital asset themed words.
+
+## Disclaimer 
+This tool may not list 100% of applicable terms related to an engagement and should be used in conjuction with a skilled team of attornies and technologists.
+
+## Additional Disclaimer
+This website is pretty simple but unfinished. Feel free to improve it if that's your thing. 
+
+## Credits
+Built (quickly) by [Joshua McDougall](https://www.linkedin.com/in/joshuamcdougall/) for [#RelativityFest](https://relativityfest.com). 
+
+Asset & Exchange data pulled directly from 🦊[Coincap.io](https://coincap.io)🦊
+
+Style elements from [NES.css](https://nostalgic-css.github.io/NES.css/)
+
---
+++ tokens.fyi.js
@@ -0,0 +1,176 @@
+
+
+
+var understandable = 0;
+
+var results = '';
+
+var keywords_base = [];
+var keywords_wallets = [];
+var keywords_assets = [];
+var keywords_exchanges = [];
+
+
+
+var test = '';
+
+$(document).ready(function() {
+
+	$('#button_generate').click(function(){
+
+		results = '';
+
+		$.getJSON( "data/keywords_base.json", function( data ) {
+
+		  $.each(data, function( key, val ) {
+		    
+		  	 	keywords_base.push(  data[key] );
+			
+		   });
+
+		}).done(function(){
+			displayResults();
+		});
+
+
+
+		$.getJSON( "https://api.coincap.io/v2/assets?limit=50", function( data ) {
+
+		  $.each(data.data, function( key, val ) {
+		    
+		    if (val.id.toUpperCase() !== val.name.toUpperCase()) {
+			    keywords_assets.push(  val.name  );
+			    keywords_assets.push(  val.id  );
+
+		    } else {
+		    	keywords_assets.push(  val.name  );
+		    }
+
+		    if (!(val.symbol == val.id || val.symbol == val.name)) { 
+		  	 	keywords_assets.push(  val.symbol  );
+			}
+		   });
+
+		}).done(function(){
+			displayResults();
+		});
+		    
+		$.getJSON( "https://api.coincap.io/v2/exchanges", function( data ) {
+
+		  $.each(data.data, function( key, val ) {
+		    
+		    if (val.exchangeId.toUpperCase() !== val.name.toUpperCase()) {
+			    keywords_exchanges.push(  val.name  );
+			    keywords_exchanges.push(  val.exchangeId  );
+
+		    } else {
+		    	keywords_exchanges.push(  val.name  );
+		    }
+
+		    keywords_exchanges.push(  val.exchangeUrl  );
+		
+		   });
+
+		}).done(function(){
+			displayResults();
+		});
+
+
+	});
+
+    
+    $('input[name=result_display_type]').change(function(){ 
+	    $('.result_display_type').removeClass('is-primary');
+	    $('#results_display_type_' + this.value + '_value').addClass('is-primary');
+
+		displayResults();
+	});
+	
+	
+	// Troll
+	$('#button_understand').click(function(){
+
+		if (understandable > 10) {
+			$('#understood').html('(lol, this is staying)');
+		} else {
+			$('#understood').html('');			
+		}
+		
+		understandable = (understandable + 1 ) % 13;
+		
+	});
+
+});
+
+
+
+
+function displayResults() {
+	
+	if ($('input[name=result_display_type]:checked').val() == 'List' ) {
+
+
+
+		if ($('#terms_base').is(':checked')) {
+			for(var i =0; i < keywords_base.length; i++){
+				if (
+						(
+							$('#colour_background').val() != '' 
+							&& $('#colour_background').val() != null 
+						)
+						|| 
+						(
+							$('#colour_text').val() != ''
+							&& $('#colour_text').val() != null
+						)
+					) {
+					results += ($('#colour_background').val() != null && $('#colour_background').val() != '')? $('#colour_background').val() +';' : 0 +';';
+					results += ($('#colour_text').val() != null && $('#colour_text').val() != '')? $('#colour_text').val() +';' : 0 +';';
+				}
+				results += keywords_base[i] + '\n';
+			}
+		}
+
+		if ($('#terms_assets').is(':checked')) {
+			for(var i =0; i < keywords_assets.length; i++){
+				if (
+						(
+							$('#colour_background').val() != '' 
+							&& $('#colour_background').val() != null 
+						)
+						|| 
+						(
+							$('#colour_text').val() != ''
+							&& $('#colour_text').val() != null
+						)
+					) {
+					results += ($('#colour_background').val() != null && $('#colour_background').val() != '')? $('#colour_background').val() +';' : 0 +';';
+					results += ($('#colour_text').val() != null && $('#colour_text').val() != '')? $('#colour_text').val() +';' : 0 +';';
+				}
+				results += keywords_assets[i] + '\n';
+			}
+		}
+
+		if ($('#terms_exchanges').is(':checked')) {
+			for(var i =0; i < keywords_exchanges.length; i++){
+				results += ($('#colour_background').val() != null)? $('#colour_background').val() +';' : 0 +';';
+				results += ($('#colour_background').val() != null)? $('#colour_text').val() +';' : 0 +';';
+				results += keywords_exchanges[i] + '\n';
+			}
+		}
+		//results = keywords_assets.join('\n') + '\n' + keywords_exchanges.join('\n');
+		$('#textarea_field').html(results);  
+		
+	} else if ($('input[name=result_display_type]:checked').val() == 'Table' ) {
+		// TODO build table version
+		
+	} 
+		
+	 
+}
+
+
+function zeroPad(num, places) {
+  var zero = places - num.toString().length + 1;
+  return Array(+(zero > 0 && zero)).join("0") + num;
+}
